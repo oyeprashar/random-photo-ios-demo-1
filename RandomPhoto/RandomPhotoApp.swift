57d67956de91d9907d345cbb0fb5e76607b11e55
shubhamprashar <shubham.prashar@finbox.in>
2024-05-14 21:59:29 +0530

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Shubham Prashar on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
